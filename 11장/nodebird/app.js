909b487a9a60d96344461b09236591bfb2bfea9b
Young Soo Kim <41531594+devYSK@users.noreply.github.com>
2021-08-17 12:13:17 +0900

    11장 학습

--- 11장/nodebird/app.js
+++ 11장/nodebird/app.js
@@ -7,30 +7,24 @@ const nunjucks = require('nunjucks');
 const dotenv = require('dotenv');
 const passport = require('passport');
 
-
+dotenv.config();
 const pageRouter = require('./routes/page');
 const authRouter = require('./routes/auth');
 const postRouter = require('./routes/post');
 const userRouter = require('./routes/user');
-
 const { sequelize } = require('./models');
 const passportConfig = require('./passport');
 
-dotenv.config();
-
 const app = express();
-passportConfig();
-
-
-app.set('port', process.env.PORT || 8002);
+passportConfig(); // 패스포트 설정
+app.set('port', process.env.PORT || 8001);
 app.set('view engine', 'html');
 nunjucks.configure('views', {
     express: app,
     watch: true,
 });
 
-
-sequelize.sync({ force: false})
+sequelize.sync({ force: false })
     .then(() => {
         console.log('데이터베이스 연결 성공');
     })
@@ -38,12 +32,11 @@ sequelize.sync({ force: false})
         console.error(err);
     });
 
-
-
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/img', express.static(path.join(__dirname, 'uploads')));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(session({
     resave: false,
@@ -54,8 +47,6 @@ app.use(session({
         secure: false,
     },
 }));
-app.use('/img', express.static(path.join(__dirname, 'uploads')));
-
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -65,22 +56,17 @@ app.use('/post', postRouter);
 app.use('/user', userRouter);
 
 app.use((req, res, next) => {
-    const error = new Error(`${req.method} ${req.url} 라우터가 없습니다`);
+    const error =  new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
     error.status = 404;
     next(error);
 });
 
 app.use((err, req, res, next) => {
+    console.error(err);
     res.locals.message = err.message;
     res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
     res.status(err.status || 500);
     res.render('error');
 });
 
-
-app.listen(app.get('port'), () => {
-    console.log(app.get('port'), '번 포트에서 대기');
-});
-
 module.exports = app;
-
